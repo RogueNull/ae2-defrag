6188ec601c569f72b79d9a0d56c294c4b239205b
MsRogue <roguehunter13@gmail.com>
2022-02-14 20:21:03 -0500

    Print items

--- defrag.lua
+++ defrag.lua
@@ -2,3 +2,10 @@ local bridge = peripheral.find("meBridge")
 local items = bridge.listItems()
 print("Bridge: ", bridge)
 print("Items: ", items)
+
+for i,item in ipairs(items) do
+  print("Item: ", item)
+  for j,line in ipairs(item) do
+    print("Line: ", line)
+  end
+end
